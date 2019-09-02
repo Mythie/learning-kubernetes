0d2425c3949a3c073941f359bbd55ef42a5959d5
Lucas Smith <mythiedev@gmail.com>
2019-09-02 18:22:31 +1000

    Refinement of apply.sh

--- apply.sh
+++ apply.sh
@@ -9,13 +9,26 @@ fi
 
 terraform apply -auto-approve
 
-terraform output config-map > config-map.yaml
-terraform output cluster-autoscaler > cluster-autoscaler.yaml
+mkdir outputs
 
-aws eks update-kubeconfig --name $(terraform output cluster-name)
-
-kubectl apply -f config-map.yaml
+terraform output config-map > ./outputs/config-map.yaml
+terraform output cluster-autoscaler > ./outputs/cluster-autoscaler.yaml
 
 echo "[SUCCESS] You're ready to start using Kubernetes"
+aws eks update-kubeconfig --name $(terraform output cluster-name)
+
+echo "[INFO] Applying Worker Node config"
+kubectl apply -f ./outputs/config-map.yaml
+echo "[INFO] Applying Cluster Autoscaler"
+kubectl apply -f ./outputs/cluster-autoscaler.yaml
+
+echo "[INFO] Waiting for workers to join cluster"
+sleep 15
 
 kubectl get svc && kubectl get nodes
+
+echo "[INFO] Deploying preconfigured deployments from ./deployments folder"
+for file in ./deployments/*.yaml; do
+  echo "[INFO] Applying $file"
+  kubectl apply -f $file
+done;
